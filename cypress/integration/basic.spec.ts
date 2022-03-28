eacfbdd3f80103cdf164d406422648ea6975d356
Peter Odetayo <peterodet@outlook.com>
2022-03-28 19:09:56 +0100

    Init

---
+++ cypress/integration/basic.spec.ts
@@ -0,0 +1,37 @@
+
+context('Basic', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('basic nav', () => {
+    cy.url()
+      .should('eq', 'http://localhost:3000/')
+
+    cy.contains('[Home Layout]')
+      .should('exist')
+
+    cy.get('#input')
+      .type('Vitesse{Enter}')
+      .url()
+      .should('eq', 'http://localhost:3000/hi/Vitesse')
+
+    cy.contains('[Default Layout]')
+      .should('exist')
+
+    cy.get('.btn')
+      .click()
+      .url()
+      .should('eq', 'http://localhost:3000/')
+  })
+
+  it('markdown', () => {
+    cy.get('[title="About"]')
+      .click()
+      .url()
+      .should('eq', 'http://localhost:3000/about')
+
+    cy.get('pre.language-js')
+      .should('exist')
+  })
+})
